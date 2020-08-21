d77be5b758dd96da51f37155b7f279b40f52603a
Semenov Vladimir Olegovich <vlsemenov@ozon.ru>
2020-08-21 20:44:48 +0300

    add mongoconfig

---
+++ venv/connect.py
@@ -0,0 +1,19 @@
+from urllib.parse import quote_plus as quote
+
+import ssl
+import pymongo
+
+url = 'mongodb://{user}:{pw}@{hosts}/?replicaSet={rs}&authSource={auth_src}'.format(
+    user=quote('user1'),
+    pw=quote('<password>'),
+    hosts=','.join([
+        'rc1c-tplly2g13saa2p6m.mdb.yandexcloud.net:27018'
+    ]),
+    rs='rs01',
+    auth_src='db1')
+dbs = pymongo.MongoClient(
+    url,
+    ssl_ca_certs='/usr/local/share/ca-certificates/Yandex/YandexInternalRootCA.crt',
+    ssl_cert_reqs=ssl.CERT_REQUIRED)['db1']
+
+# dbs.test_collection.find(...)
